c14e04946b9e5fbcfecb21bb0c5704b3f08196ac
Antonio Ángeles <aangeles@futurespace.es>
2026-03-13 12:36:37 +0100

    Creación de repositorios con Spring Data JPA

---
+++ spring-data-jpa/src/main/java/com/angeles/spring_data_jpa/repository/LibroRepository.java
@@ -0,0 +1,27 @@
+package com.angeles.spring_data_jpa.repository;
+
+import com.angeles.spring_data_jpa.entity.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface LibroRepository extends JpaRepository<Libro, Integer> {
+
+    // Ejercicio 4
+    @Query("SELECT l FROM Libro l WHERE l.anioPublicacion > 2001")
+    List<Libro> librosPublicadosDespuesDe2001();
+
+    // Ejercicio 5
+    List<Libro> findByAnioPublicacionGreaterThan(Integer anio);
+
+    // Ejercicio 6
+    List<Libro> findByAnioPublicacion(Integer anio);
+
+    Libro findByIsbn(String isbn);
+
+    List<Libro> findByIdEditorial(Integer idEditorial);
+
+    List<Libro> findByIdEditorialAndAnioPublicacion(Integer editorial, Integer anio);
+}
+
